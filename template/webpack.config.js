60191adcae73efa74a442d98b0ad0cdd0786f3fe
Takao Naito <xarsh@outlook.com>
2018-07-17 21:30:45 +0900

    Update webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -40,10 +40,8 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.mode = 'production'
-  module.exports.devtool = '#source-map'
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({'process.env': {NODE_ENV: '"production"'}}),
-    new webpack.optimize.UglifyJsPlugin({sourceMap: true, compress: {warnings: false}}),
     new webpack.LoaderOptionsPlugin({minimize: true})
   ])
 }
